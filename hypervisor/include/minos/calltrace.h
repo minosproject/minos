e7b0254c1bb3da8899a00ab858e0f70b95e18860
Baodong Chen <chenbaodong@mxnavi.com>
2019-02-26 16:21:53 +0800

    add '__noreturn' in 'minos/compiler.h'
    Signed-off-by: Baodong Chen <chenbaodong@mxnavi.com>

--- hypervisor/include/minos/calltrace.h
+++ hypervisor/include/minos/calltrace.h
@@ -1,7 +1,9 @@
 #ifndef __MINOS_CALLTRACE_H_
 #define __MINOS_CALLTRACE_H_
 
-void __panic(gp_regs *regs, char *str, ...);
+#include <minos/compiler.h>
+
+void __panic(gp_regs *regs, char *str, ...) __noreturn;
 void print_symbol(unsigned long addr);
 void dump_stack(gp_regs *regs, unsigned long *stack);
 
