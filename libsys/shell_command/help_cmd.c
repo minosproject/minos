fa8d38a64d97400197dc04e8c2057c04734be0fa
Lei Feng <l3ifeng@outlook.com>
2020-11-17 16:26:46 +0800

    add "help" command to print all commands
    Signed-off-by: Lei Feng <l3ifeng@outlook.com>

---
+++ libsys/shell_command/help_cmd.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <minos/minos.h>
+#include <minos/shell_command.h>
+
+extern unsigned long __shell_command_start;
+extern unsigned long __shell_command_end;
+
+static int
+help_cmd(int argc, char** argv)
+{
+    struct shell_command* cmd;
+    char* spaces = "       ";
+
+    section_for_each_item(__shell_command_start, __shell_command_end, cmd) {
+        printf("%s%s - %s\n",
+               cmd->name,
+               spaces + (MIN(strlen(cmd->name), strlen(spaces))),
+               cmd->cmd_info);
+    }
+
+    return 0;
+}
+
+DEFINE_SHELL_COMMAND(help, "help", "print command description", help_cmd, 0);
