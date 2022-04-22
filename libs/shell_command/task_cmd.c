0997340b7a4598e27099b6bce8b898ce07b2ac65
Min Le <lemin9538@gmail.com>
2022-04-22 23:24:55 +0800

    print the state string of the task when ps
    Signed-off-by: Min Le <lemin9538@gmail.com>

--- libs/shell_command/task_cmd.c
+++ libs/shell_command/task_cmd.c
@@ -18,6 +18,36 @@
 #include <minos/shell_command.h>
 #include <virt/vm.h>
 
+static char *state_str[7] = {
+	"Running",
+	"  Ready",
+	"   Wait",
+	" Waking",
+	"Suspend",
+	"Stopped",
+	"  Wrong",
+};
+
+static inline char *get_state_str(struct task *task)
+{
+	switch (task->state) {
+	case TASK_STATE_RUNNING:
+		return state_str[0];
+	case TASK_STATE_READY:
+		return state_str[1];
+	case TASK_STATE_WAIT_EVENT:
+		return state_str[2];
+	case TASK_STATE_WAKING:
+		return state_str[3];
+	case TASK_STATE_SUSPEND:
+		return state_str[4];
+	case TASK_STATE_STOP:
+		return state_str[5];
+	}
+
+	return state_str[6];
+}
+
 static void dump_task_info(struct task *task)
 {
 	char vm_str[8] = {0};
@@ -28,13 +58,13 @@ static void dump_task_info(struct task *task)
 		sprintf(vm_str, "vm-%d/", vcpu->vm->vmid);
 	}
 
-	printf("%4d %3d %4d %s%s\n", task->tid, task->cpu,
-			task->state, vm_str, task->name);
+	printf("%4d %3d %s %s%s\n", task->tid, task->cpu,
+			get_state_str(task), vm_str, task->name);
 }
 
 static int ps_cmd(int argc, char **argv)
 {
-	printf(" PID CPU STAT NAME\n");
+	printf(" PID CPU   STATE NAME\n");
 	os_for_all_task(dump_task_info);
 
 	return 0;
